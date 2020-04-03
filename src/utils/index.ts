fa47ac21c87429516d5a114b7f593d30a7934a5f
merlin.ho <merlin.ho@kakakocorp.com>
2020-04-03 10:04:16 +0900

    Modify scrollMoveTo 버그 수정

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,26 +2,30 @@ export function scrollMoveTo(
   wrapperElement: HTMLDivElement,
   currentIndex: number
 ) {
-  const wrapperWidth = wrapperElement.offsetWidth;
-  const wrapperMidPos = wrapperWidth / 2;
-  const wrapperLeftOffset = wrapperElement.offsetLeft;
+  let startX = 0;
 
-  const $elemCurrentMenu = wrapperElement.children[0].children[
-    currentIndex
-  ] as HTMLLIElement;
-  const $elemMenuWrapperWidth = (wrapperElement.children[0] as HTMLUListElement)
-    .offsetWidth;
-  const currenttMidWidth = $elemCurrentMenu.offsetWidth / 2;
-  const currentLeftOffset = $elemCurrentMenu.offsetLeft;
-  const maxWidth = $elemMenuWrapperWidth - wrapperWidth;
+  if (currentIndex > -1) {
+    const wrapperWidth = wrapperElement.offsetWidth;
+    const wrapperMidPos = wrapperWidth / 2;
+    const wrapperLeftOffset = wrapperElement.offsetLeft;
 
-  let startX =
-    wrapperLeftOffset - currentLeftOffset + wrapperMidPos - currenttMidWidth;
+    const $elemCurrentMenu = wrapperElement.children[0].children[
+      currentIndex
+    ] as HTMLLIElement;
+    const $elemMenuWrapperWidth = (wrapperElement
+      .children[0] as HTMLUListElement).offsetWidth;
+    const currenttMidWidth = $elemCurrentMenu.offsetWidth / 2;
+    const currentLeftOffset = $elemCurrentMenu.offsetLeft;
+    const maxWidth = $elemMenuWrapperWidth - wrapperWidth;
 
-  if (startX > 0) {
-    startX = 0;
-  } else if (startX < -maxWidth) {
-    startX = -maxWidth;
+    startX =
+      wrapperLeftOffset - currentLeftOffset + wrapperMidPos - currenttMidWidth;
+
+    if (startX > 0) {
+      startX = 0;
+    } else if (startX < -maxWidth) {
+      startX = -maxWidth;
+    }
   }
 
   return startX;
